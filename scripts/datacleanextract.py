aa49086e3f643e14a451cddecb416b6823e20829
shivanXI <shivanstar7@gmail.com>
2017-07-16 17:29:09 +0530

    Added scripts for feature extraction-19

--- scripts/datacleanextract.py
+++ scripts/datacleanextract.py
@@ -81,4 +81,7 @@ def get_required_features(label_keys = REQUIRED_LABEL_KEYS):
         writer = csv.writer(f, delimiter=',', lineterminator='\n')
         writer.writerows(features_data)
     
-                
+
+if __name__=='__main__':
+    get_required_features()
+               
