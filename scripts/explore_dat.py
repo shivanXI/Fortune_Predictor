2cfd6c01a0848cc0ff682cfbe0bf2b0adccc512a
shivanXI <shivanstar7@gmail.com>
2017-07-16 16:59:50 +0530

    Added scripts for data handeling and exploration for applying models for testing-01

--- scripts/explore_dat.py
+++ scripts/explore_dat.py
@@ -1,3 +1,8 @@
 from collections import Counter
 from matplotlib import pyplot as plt
 import csv
+
+DICTIONARY_FILENAME = 'data/CollegeScorecardDataDictionary-09-12-2015.csv'
+DATA_FILENAME = 'data/MERGED2011_PP.csv'
+NAME_KEY_INDEX = 3
+
