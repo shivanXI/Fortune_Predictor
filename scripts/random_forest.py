77cca5e10d8e390ea63f579498496c005b505cf7
shivanXI <shivanstar7@gmail.com>
2017-07-16 17:53:18 +0530

    Added scripts random_forest for classification of data fetched-09

--- scripts/random_forest.py
+++ scripts/random_forest.py
@@ -91,3 +91,18 @@ def gini_index(groups, class_values):
 			gini += (proportion * (1.0 - proportion))
 	return gini
  
+def get_split(dataset, n_features):
+	class_values = list(set(row[-1] for row in dataset))
+	b_index, b_value, b_score, b_groups = 999, 999, 999, None
+	features = list()
+	while len(features) < n_features:
+		index = randrange(len(dataset[0])-1)
+		if index not in features:
+			features.append(index)
+	for index in features:
+		for row in dataset:
+			groups = test_split(index, row[index], dataset)
+			gini = gini_index(groups, class_values)
+			if gini < b_score:
+				b_index, b_value, b_score, b_groups = index, row[index], gini, groups
+	return {'index':b_index, 'value':b_value, 'groups':b_groups}
