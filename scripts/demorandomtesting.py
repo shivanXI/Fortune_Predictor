10bd2ec6a72f6aa1fab0afe8a9938452713d3a8b
shivanXI <shivanstar7@gmail.com>
2017-07-16 17:23:26 +0530

    Added scripts for feature extraction-15

--- scripts/demorandomtesting.py
+++ scripts/demorandomtesting.py
@@ -6,3 +6,13 @@ test = pd.read_csv("train_feature_data.csv")
 default = pd.read_csv("default_outcome_data.csv")
 cols = [0, 1, 2, 3]
 colres = [0]
+
+
+#label_indices = [keys.index(label_key) for label_key in label_keys]
+
+trainArr = train.as_matrix(cols)
+trainRes = train.as_matrix(colres)
+
+
+rf = RandomForestClassifier(n_estimators=100)
+rf.fit(trainArr, trainRes)
