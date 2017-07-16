7d71855c3a5ab748a3de2759028b6231a475cd32
shivanXI <shivanstar7@gmail.com>
2017-07-16 17:51:02 +0530

    Added scripts random_forest for classification of data fetched-03

--- scripts/random_forest.py
+++ scripts/random_forest.py
@@ -16,3 +16,13 @@ def str_column_to_float(dataset, column):
 	for row in dataset:
 		row[column] = float(row[column].strip())
  
+
+def str_column_to_int(dataset, column):
+	class_values = [row[column] for row in dataset]
+	unique = set(class_values)
+	lookup = dict()
+	for i, value in enumerate(unique):
+		lookup[value] = i
+	for row in dataset:
+		row[column] = lookup[row[column]]
+	return lookup
