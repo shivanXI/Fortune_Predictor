654c39d7c6ce19b1a9e2e2d5c635342d7d7cc241
shivanXI <shivanstar7@gmail.com>
2017-07-16 17:08:18 +0530

    Added scripts for feature extraction-01

--- scripts/feature_extraction.py
+++ scripts/feature_extraction.py
@@ -1,2 +1,14 @@
 import csv
 import read_dataset
+
+
+DICTIONARY_FILENAME = 'MERGED2011_12_PP.csv'
+
+LABEL_KEYS = [
+    'GRAD_DEBT_MDN',
+    'MD_EARN_WNE_P6',
+]
+
+REQUIRED_LABEL_KEY = [
+    'MN_EARN_WNE_INC1_P10',
+]
