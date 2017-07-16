53cead9c2dfb19f826ec5f0c5b273b52e56b6c77
shivanXI <shivanstar7@gmail.com>
2017-07-16 17:00:35 +0530

    Added scripts for data handeling and exploration for applying models for testing-02

--- scripts/explore_dat.py
+++ scripts/explore_dat.py
@@ -6,3 +6,5 @@ DICTIONARY_FILENAME = 'data/CollegeScorecardDataDictionary-09-12-2015.csv'
 DATA_FILENAME = 'data/MERGED2011_PP.csv'
 NAME_KEY_INDEX = 3
 
+def is_null(row, key, count_private_as_null = True):
+	return row[key] == 'NULL' or (count_private_as_null and row[key] == 'PrivacySuppressed')
