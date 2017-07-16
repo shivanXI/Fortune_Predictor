c12a12f3f4d671969b82f045bc62976dd2ff3a0a
shivanXI <shivanstar7@gmail.com>
2017-07-16 17:51:21 +0530

    Added scripts random_forest for classification of data fetched-04

--- scripts/random_forest.py
+++ scripts/random_forest.py
@@ -26,3 +26,16 @@ def str_column_to_int(dataset, column):
 	for row in dataset:
 		row[column] = lookup[row[column]]
 	return lookup
+
+
+def cross_validation_split(dataset, n_folds):
+	dataset_split = list()
+	dataset_copy = list(dataset)
+	fold_size = int(len(dataset) / n_folds)
+	for i in range(n_folds):
+		fold = list()
+		while len(fold) < fold_size:
+			index = randrange(len(dataset_copy))
+			fold.append(dataset_copy.pop(index))
+		dataset_split.append(fold)
+	return dataset_split
