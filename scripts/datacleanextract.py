ca52c027f8f271ecbd09b882bb32e272a98c2e30
shivanXI <shivanstar7@gmail.com>
2017-07-16 17:26:36 +0530

    Added scripts for feature extraction-17

--- scripts/datacleanextract.py
+++ scripts/datacleanextract.py
@@ -2,3 +2,13 @@ import csv
 import math
 import codecs
 import read_dataset
+
+DEFAULT_DATASET_NAME = 'MERGED2011_12_PP.csv'
+
+REQUIRED_LABEL_KEYS = [
+    'GRAD_DEBT_MDN',
+    'MD_EARN_WNE_P6',
+    'CIP01ASSOC',
+    'PCIP01',
+    'MN_EARN_WNE_INC1_P10',
+]
