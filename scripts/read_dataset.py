4bf917ac6ee0584cdf16d406b0106ce4258a8738
shivanXI <shivanstar7@gmail.com>
2017-07-16 17:32:18 +0530

    Added scripts for reading dataset and filtering required rows-02

--- scripts/read_dataset.py
+++ scripts/read_dataset.py
@@ -16,3 +16,15 @@ DEFAULT_REQUIRED_KEYS = [
 
 def is_null(row, key_index, count_private_as_null = True):
 	return row[key_index] == 'NULL' or (count_private_as_null and row[key_index] == 'PrivacySuppressed')
+
+
+def get_all_rows(data_file_name=DEFAULT_DATASET_NAME):
+    with open(data_file_name, 'r') as data_file:
+        # reader = csv.reader(data_file)
+        # rows = [row for row in csv.reader(data_file)]
+        rows = data_file.readlines()
+        for i in xrange(len(rows)):
+            if rows[i].startswith(codecs.BOM_UTF8):
+                rows[i] = rows[i][len(codecs.BOM_UTF8):]
+            rows[i] = rows[i].strip().split(',')
+        return rows[1:], rows[0]
