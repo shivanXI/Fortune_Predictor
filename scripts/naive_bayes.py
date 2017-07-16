420faa228550b57f9e6fb1c1aa5a7f2d5fdd35b7
shivanXI <shivanstar7@gmail.com>
2017-07-16 17:43:42 +0530

    Added scripts naive_bayes for classification of data fetched according to features-07

--- scripts/naive_bayes.py
+++ scripts/naive_bayes.py
@@ -45,3 +45,9 @@ def summarize(dataset):
 	del summaries[-1]
 	return summaries
 
+def summarizeByClass(dataset):
+	separated = separateByClass(dataset)
+	summaries = {}
+	for classValue, instances in separated.items():
+		summaries[classValue] = summarize(instances)
+	return summaries
