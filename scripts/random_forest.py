181ca3b565e7c08518e1b1506d19f4e7c2c043c1
shivanXI <shivanstar7@gmail.com>
2017-07-16 17:54:00 +0530

    Added scripts random_forest for classification of data fetched-11

--- scripts/random_forest.py
+++ scripts/random_forest.py
@@ -110,3 +110,30 @@ def get_split(dataset, n_features):
 def to_terminal(group):
 	outcomes = [row[-1] for row in group]
 	return max(set(outcomes), key=outcomes.count)
+
+
+def split(node, max_depth, min_size, n_features, depth):
+	left, right = node['groups']
+	del(node['groups'])
+	# check for a no split
+	if not left or not right:
+		node['left'] = node['right'] = to_terminal(left + right)
+		return
+	# check for max depth
+	if depth >= max_depth:
+		node['left'], node['right'] = to_terminal(left), to_terminal(right)
+		return
+	# process left child
+	if len(left) <= min_size:
+		node['left'] = to_terminal(left)
+	else:
+		node['left'] = get_split(left, n_features)
+		split(node['left'], max_depth, min_size, n_features, depth+1)
+	# process right child
+	if len(right) <= min_size:
+		node['right'] = to_terminal(right)
+	else:
+		node['right'] = get_split(right, n_features)
+		split(node['right'], max_depth, min_size, n_features, depth+1)
+ 
+
