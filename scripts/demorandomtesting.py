8449e5719071e97d234750520730e5d1a5708c7b
shivanXI <shivanstar7@gmail.com>
2017-07-16 17:22:25 +0530

    Added scripts for feature extraction-14

--- scripts/demorandomtesting.py
+++ scripts/demorandomtesting.py
@@ -1,3 +1,8 @@
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 
+train = pd.read_csv("train_feature_data.csv")
+test = pd.read_csv("train_feature_data.csv")
+default = pd.read_csv("default_outcome_data.csv")
+cols = [0, 1, 2, 3]
+colres = [0]
