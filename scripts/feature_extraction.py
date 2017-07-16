c10f7c7bcebda34a8939f3a55af93b4cbb7d5c63
shivanXI <shivanstar7@gmail.com>
2017-07-16 17:15:35 +0530

    Added scripts for feature extraction-08

--- scripts/feature_extraction.py
+++ scripts/feature_extraction.py
@@ -134,3 +134,72 @@ def get_examples(label_keys=LABEL_KEYS):
 
     return examples, sorted(features), label_keys, privacy_suppressed_values, sorted(privacy_suppressed_features)
 
+
+def get_required_label(label_keys=REQUIRED_LABEL_KEY):
+    
+    rows, keys = read_dataset.get_filtered_rows()
+
+    with open(DICTIONARY_FILENAME, 'r') as dict_file:
+        key_rows = [row for row in csv.reader(dict_file)][1:]
+    key_row_lookup = {key_row[4]: key_row for key_row in key_rows if key_row[4]}
+
+    non_feature_keys = get_non_feature_keys(rows, keys, key_row_lookup)
+    privacy_suppressed_keys = set([
+        keys[i] for i in xrange(len(keys))
+        if any([row[i] == 'PrivacySuppressed' for row in rows])
+    ])
+    #categorical_keys = get_categorical_keys(key_rows)
+
+    label_indices = [keys.index(label_key) for label_key in label_keys]
+
+    #examples = []
+    #privacy_suppressed_values = []
+
+    for row in rows:
+        features = {}
+        #privacy_suppressed_features = {}
+        for i in xrange(len(keys)):
+            if keys[i] not in non_feature_keys:
+                if keys[i] in privacy_suppressed_keys:
+                    if is_null(row, i):
+                        value = 0.0
+                    elif row[i] == 'PrivacySuppressed':
+                        value = -1.0
+                    else:
+                        value = row[i]
+                    #privacy_suppressed_features[keys[i]] = value
+                else:
+                    is_null_key = '%s_is_NULL' % (keys[i])
+
+                    #if keys[i] in categorical_keys:
+                        #category_value_is_null = is_null(row, i)
+                        #features[is_null_key] = 1.0 if category_value_is_null else 0.0
+                        #for category_value, category_label in categorical_keys[keys[i]]:
+                            #category_key = '%s = %s' % (keys[i], category_label)
+                            #features[category_key] = (
+                                #1.0 if not category_value_is_null and row[i] == category_value
+                                #else 0.0
+                            #)
+
+                    #else: # Non-categorical keys
+                        # TODO: alternative ways of dealing with PrivacySuppressed?
+                    if is_null(row, i):
+                        features[keys[i]] = 0.0
+                        features[is_null_key] = 1.0
+                    else:
+                        features[keys[i]] = row[i]
+                        features[is_null_key] = 0.0
+
+        # Arrange features alphabetically for more consistent ordering
+        # between runs and easier exploration of the fitted model
+        feature_values = [features[key] for key in sorted(features)]
+        try:
+            labels = [float(row[label_index]) for label_index in label_indices]
+        except ValueError:
+            labels = [row[label_index].strip() for label_index in label_indices]
+        #examples.append((feature_values, labels))
+        #privacy_suppressed_values.append([privacy_suppressed_features[key] for key in sorted(privacy_suppressed_features)])
+
+    return feature_values
+
+#print get_required_label()
