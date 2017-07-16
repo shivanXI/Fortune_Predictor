4d7882f668ebe3bd6b01b1f82b23a5c0e274a3c3
shivanXI <shivanstar7@gmail.com>
2017-07-16 17:50:25 +0530

    Added scripts random_forest for classification of data fetched-01

--- scripts/random_forest.py
+++ scripts/random_forest.py
@@ -3,3 +3,12 @@ from random import randrange
 from csv import reader
 from math import sqrt
  
+def load_csv(filename):
+	dataset = list()
+	with open(filename, 'r') as file:
+		csv_reader = reader(file)
+		for row in csv_reader:
+			if not row:
+				continue
+			dataset.append(row)
+	return dataset
