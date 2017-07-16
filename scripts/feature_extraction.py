bc48003bf1bc64e84ea091ae3270be2a80629635
shivanXI <shivanstar7@gmail.com>
2017-07-16 17:09:45 +0530

    Added scripts for feature extraction-03

--- scripts/feature_extraction.py
+++ scripts/feature_extraction.py
@@ -15,3 +15,10 @@ REQUIRED_LABEL_KEY = [
 
 def is_null(row, key_index, count_private_as_null=False):
     return row[key_index] == 'NULL' or (count_private_as_null and row[key_index] == 'PrivacySuppressed')
+
+
+def is_prediction_key(key_row):
+    # 'aid' fields that are not prediction keys
+    if key_row[0] in ['pell_grant_rate', 'PCTFLOAN', 'loan_ever']:
+        return True
+    return key_row[2] in ['repayment', 'earnings', 'aid']
