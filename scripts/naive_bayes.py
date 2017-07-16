1976f8e1c54df14f1584ecb3a83ed84be0bcbaa2
shivanXI <shivanstar7@gmail.com>
2017-07-16 17:40:18 +0530

    Added scripts naive_bayes for classification of data fetched according to features-01

--- scripts/naive_bayes.py
+++ scripts/naive_bayes.py
@@ -2,3 +2,11 @@ from __future__ import division
 import csv
 import random
 import math
+
+
+def loadCsv(filename):
+	lines = csv.reader(open(filename, "rt"))
+	dataset = list(lines)
+	for i in range(len(dataset)):
+		dataset[i] = [float(x) for x in dataset[i]]
+	return dataset
