b1746b40971316f2714703e98b073dd0f48d40c0
shivanXI <shivanstar7@gmail.com>
2017-07-16 17:54:53 +0530

    Added scripts random_forest for classification of data fetched-13

--- scripts/random_forest.py
+++ scripts/random_forest.py
@@ -140,5 +140,17 @@ def split(node, max_depth, min_size, n_features, depth):
 	root = get_split(train, n_features)
 	split(root, max_depth, min_size, n_features, 1)
 	return root
+
+def predict(node, row):
+	if row[node['index']] < node['value']:
+		if isinstance(node['left'], dict):
+			return predict(node['left'], row)
+		else:
+			return node['left']
+	else:
+		if isinstance(node['right'], dict):
+			return predict(node['right'], row)
+		else:
+			return node['right']
  
 
