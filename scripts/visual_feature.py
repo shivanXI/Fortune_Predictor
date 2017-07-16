c00512c6ba2f95c4bc943f1a24e6f781c765f8d8
shivanXI <shivanstar7@gmail.com>
2017-07-16 17:36:03 +0530

    Added scripts for reading dataset and filtering required rows and features-02

--- scripts/visual_feature.py
+++ scripts/visual_feature.py
@@ -19,3 +19,6 @@ REQUIRED_INCOME_KEYS = [
     'MN_EARN_WNE_MALE0_P10',
     'MN_EARN_WNE_MALE1_P10'
 ]
+
+def is_null(row, key_index, count_private_as_null=False):
+    return row[key_index] == 'NULL' or (count_private_as_null and row[key_index] == 'PrivacySuppressed')
